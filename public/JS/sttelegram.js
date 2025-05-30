71a029bda626b205cb17cf2c3686977fa3fc32a8
Thangchiv Khov <icetea0423@gmail.com>
2025-05-30 11:39:05 +0700

    update

--- public/JS/sttelegram.js
+++ public/JS/sttelegram.js
@@ -1,6 +1,5 @@
 function formatTelegramMessage(order) {
-  let message = `🧾 <b>New Order Received!</b>\n`;
-  message += `━━━━━━━━━\n\n`;
+  let message = `🧾 <b>New Order Received!</b>\n\n`;
   message += ` <b>Order ID:</b> ${order.id}\n`;
   message += ` <b>Date:</b> ${new Date(order.date).toLocaleString()}\n\n`;
   message += ` <b>Items:</b>\n`;
